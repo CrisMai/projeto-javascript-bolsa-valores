57a50f066ec354a84c45f8feb97e3b54abcae8fb
CrisMai <crismai.dev@gmail.com>
2022-10-07 16:45:55 -0300

    Arrow Functions

--- client/js/app/controllers/NegociacaoController.js
+++ client/js/app/controllers/NegociacaoController.js
@@ -1,22 +1,27 @@
 
 class NegociacaoController {
 
-    constructor() {
+  constructor() {
+    let $ = document.querySelector.bind(document);
+    this._inputData = $("#data");
+    this._inputQuantidade = $("#quantidade");
+    this._inputValor = $("#valor");
+  }
 
-        let $ = document.querySelector.bind(document); 
-        this._inputData = $("#data");
-        this._inputQuantidade = $("#quantidade");
-        this._inputValor = $("#valor");
+  adiciona(event) {
+    event.preventDefault();
 
-    }
+    let data = new Date(... //(...) spread operator indica que o array será desmembrado.
+        this._inputData.value.split("-").map((item, indice) => item - indice % 2)); 
 
-    adiciona(event) {
+    let negociacao = new Negociacao(
+        data,
+        this._inputQuantidade.value,
+        this._inputValor
+    );
 
-        event.preventDefault();
+    console.log(negociacao);
 
-        let data = new Date(this._inputData.value.split('-'));
-        //let data = new Date(this._inputData.value.replace(/-/g, ','));
-        console.log(data); 
-       
-    }
+  }
+  
 }
