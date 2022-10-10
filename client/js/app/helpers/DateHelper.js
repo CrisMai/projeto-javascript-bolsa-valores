0323d0973d4a8c5c1dcfbfeb63878b86260a1bd9
CrisMai <crismai.dev@gmail.com>
2022-10-10 16:48:33 -0300

    Template strings

--- client/js/app/helpers/DateHelper.js
+++ client/js/app/helpers/DateHelper.js
@@ -1,19 +1,22 @@
 
 class DateHelper {
 
-    constructor() {
-        throw new Error("DateHelper não pode ser instanciaada");
-    }
+  constructor() {
+    throw new Error("Esta clase não pode ser instanciada");
+  }
 
-    static textoParaData(texto) {
-        return new Date(...texto.split("-").map((item, indice) => item - indice % 2));
+  static dataParaTexto(data) {
+    
+    return `${data.getDate()}/${data.getMonth() + 1}/${data.getFullYear()}`;
+  }
 
-    }
+  static textoParaData(texto) {
 
-    static dataParaTexto(data) {
+    if (!/\d{4}-\d{2}-\d{2}/.test(texto)) //expressão regular para validar data
+      throw new Error("Deve estar no formato aaaa-mm-dd");
 
-        return data.getDate() + "/" + (data.getMonth() + 1) + "/" + data.getFullYear();
-
-    }
-
+    return new Date(
+      ...texto.split("-").map((item, indice) => item - (indice % 2))
+    );
+  }
 }
