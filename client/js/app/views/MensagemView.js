00f290f2e400b76b1c2a8681d5fb0c4bb3428ac6
CrisMai <crismai.dev@gmail.com>
2022-10-13 16:55:40 -0300

    Construtor vs super

--- client/js/app/views/MensagemView.js
+++ client/js/app/views/MensagemView.js
@@ -5,9 +5,8 @@ class MensagemView extends View {
         super(elemento);
     }
 
-    _template(model) {
+    template(model) {
         return model.texto ? `<p class="alert alert-info">${model.texto}</p>` : '<p></p>';
     }
-
-   
+ 
 }
